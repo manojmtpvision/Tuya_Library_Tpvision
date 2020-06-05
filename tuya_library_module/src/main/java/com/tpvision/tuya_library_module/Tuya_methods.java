a79d4c9bd01c8a24573fdfdee74e81b28e15c716
manojmtpvision <manoj.m@tpv-tech.com>
2020-06-05 21:14:39 +0530

    Just added classes

---
+++ tuya_library_module/src/main/java/com/tpvision/tuya_library_module/Tuya_methods.java
@@ -0,0 +1,11 @@
+package com.tpvision.tuya_library_module;
+
+import android.util.Log;
+
+public class Tuya_methods {
+    public static final  String TAG="Excellent App";
+    public static void d(String message){
+        Log.d(TAG,message);
+
+    }
+}
